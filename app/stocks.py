dcc60f7f164c769a78b8447885db8653a0410688
Krish Sarawgi <krishpsarawgi@gmail.com>
2022-04-04 19:23:36 -0400

    refractoring data fetching code

--- app/stocks.py
+++ app/stocks.py
@@ -1,25 +1,17 @@
-
+# stocks.py
 
 print("STOCKS REPORT...")
 
-import os
-from dotenv import load_dotenv
-from pandas import read_csv
-
-load_dotenv()
-
-ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default="demo")
+from app.utils import to_usd
+from alphavantage_service import fetch_stocks_data
 
 symbol = input("Please input a crypto symbol (default: 'NFLX'): ") or "NFLX"
-url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={ALPHAVANTAGE_API_KEY}&datatype=csv"
 
-df = read_csv(url)
-#print(df.columns)
-#breakpoint()
+df = fetch_stocks_data(symbol)
 
 latest = df.iloc[0]
 
 print(symbol)
 print(latest["timestamp"])
 print(latest["close"])
-print('${:,.2f}'.format(latest["close"]))
+print(to_usd(format(latest["close"])))
